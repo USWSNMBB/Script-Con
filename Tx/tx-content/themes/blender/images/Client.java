abca533aa33868e5572730dcc3bbd4a1303f21ea
Ronald Warner <bombwing14@yahoo.com>
2014-11-09 19:53:10 -0800

    20141109 - Updates to targeting list

--- Tx/tx-content/themes/blender/images/Client.java
+++ Tx/tx-content/themes/blender/images/Client.java
@@ -65,3 +65,4 @@ class Client
 
 
 
+85% of millennials say they prefer urban-style living, and 68 percent of college-educated 25- to 34-year-olds say, first, they look for the place they want to live, then they look for a job.
