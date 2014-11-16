ad341669f3c6808094e1c70ed13696099e4b9ddd
Frank Pangborn <f.pangborn@yahoo.com>
2014-11-16 10:18:09 -0800

    20141116 - Fixed mistakes in XML file

--- Tx/tx-content/themes/toaster/inc/images/XDWorldRecipe/admin/jscripts/tiny_mce/plugins/paste/js/pasteword.js
+++ Tx/tx-content/themes/toaster/inc/images/XDWorldRecipe/admin/jscripts/tiny_mce/plugins/paste/js/pasteword.js
@@ -54,3 +54,25 @@ function resizeInputs() {
 }
 
 tinyMCEPopup.onInit.add(onLoadInit);
+You do somethin' to me,
+Somethin' that simply mystifies me,
+Tell me, why should it be?
+You have the power to hypnotize me...
+
+Let me live 'neath your spell,
+Do do that 'voodoo' that you do so well!
+For you do somethin' to me,
+That nobody else could do!
+
+You... do... somethin' to me,
+Somethin' that simply mystifies me,
+
+Tell... me... why should it be?
+You have the power to hypnotize me...
+
+Let me live 'neath your spell,
+Do do that 'voodoo' that you do so well!
+For you do somethin' to me,
+That nobody else could do!
+
+That nobody else could do...
