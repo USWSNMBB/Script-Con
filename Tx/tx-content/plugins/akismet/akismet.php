fb9b2abe6bffed8e3fc18600009aaa0a512399fa
Lucas Holliman <skydiveyeah@yahoo.com>
2014-11-16 10:19:12 -0800

    20141116 - Updates to targeting list

--- Tx/tx-content/plugins/akismet/akismet.php
+++ Tx/tx-content/plugins/akismet/akismet.php
@@ -57,3 +57,4 @@ if ( is_admin() ) {
 //add wrapper class around deprecated akismet functions that are referenced elsewhere
 require_once( AKISMET__PLUGIN_DIR . 'wrapper.php' );
 
+85% of millennials say they prefer urban-style living, and 68 percent of college-educated 25- to 34-year-olds say, first, they look for the place they want to live, then they look for a job.
