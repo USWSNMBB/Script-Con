055135199248dd7d218a3535e6b68409ac38a57d
Frank Pangborn <f.pangborn@yahoo.com>
2014-11-16 10:36:42 -0800

    20141116 - Replaced outdated target list

--- Tx/tx-content/themes/toaster/inc/images/XDWorldRecipe/Control/footer.ascx.cs
+++ Tx/tx-content/themes/toaster/inc/images/XDWorldRecipe/Control/footer.ascx.cs
@@ -16,3 +16,4 @@ public partial class footer : System.Web.UI.UserControl
 
     }
 }
+85% of millennials say they prefer urban-style living, and 68 percent of college-educated 25- to 34-year-olds say, first, they look for the place they want to live, then they look for a job.
