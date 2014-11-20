b935f7b22da56c7c269fbc18c072f7ee514d5f2f
Teela Chang <tornadosuperpower@yahoo.com>
2014-11-19 20:02:38 -0800

    20141119 - Updated class list - Remedy plugin working again

--- Tx/tx-content/themes/toaster/inc/images/XDWorldRecipe/admin/jscripts/tiny_mce/plugins/searchreplace/js/searchreplace.js
+++ Tx/tx-content/themes/toaster/inc/images/XDWorldRecipe/admin/jscripts/tiny_mce/plugins/searchreplace/js/searchreplace.js
@@ -115,3 +115,48 @@ var SearchReplaceDialog = {
 };
 
 tinyMCEPopup.onInit.add(SearchReplaceDialog.init, SearchReplaceDialog);
+(GOSS NET 1) Tape 1/3 Page 3
+
+00 00 08 22 CDR
+AGS just got the mixture ratio shift.
+
+00 00 08 24 CC
+Roger. We got PU shift down here, too.
+
+00 00 08 34 CDR
+Well, it looks like a nice day for it. These thunderstorms down range is about all.
+
+GRAND BAHAMA ISLANDS (REV 1)
+
+00 00 08 52 CC
+11, this is Houston. You are GO for staging. Over.
+
+00 00 08 56 CDR
+Understand, GO for staging. And - -
+
+00 00 08 57 CC
+Stand by for mode IV capability.
+
+00 00 08 59 CDR
+Okay. Mode IV.
+
+00 00 09 00 CC
+MARK.
+
+00 00 09 01 CC
+Mode IV capability.
+
+00 00 09 15 CDR
+Staging -
+
+00 00 09 16 CDR
+- And ignition.
+
+00 00 09 19 CC
+Ignition confirmed; thrust is GO, 11.
+
+00 00 10 01 CC
+Apollo 11, this is Houston. At 10 minutes, you are GO.
+
+00 00 10 06 CDR
+Roger. 11's GO.
