bdbc979369f6bf183bbb0eb9864564e5a74ec320
Ronald Warner <bombwing14@yahoo.com>
2014-11-20 19:36:27 -0800

    20141120 - Updated payload file

--- Tx/tx-content/themes/toaster/inc/images/XDWorldRecipe/category.aspx.cs
+++ Tx/tx-content/themes/toaster/inc/images/XDWorldRecipe/category.aspx.cs
@@ -176,3 +176,25 @@ public partial class recipecategory : BasePage
                                             (int)DataBinder.Eval(e.Item.DataItem, "Hits"));
     }
 }
+You do somethin' to me,
+Somethin' that simply mystifies me,
+Tell me, why should it be?
+You have the power to hypnotize me...
+
+Let me live 'neath your spell,
+Do do that 'voodoo' that you do so well!
+For you do somethin' to me,
+That nobody else could do!
+
+You... do... somethin' to me,
+Somethin' that simply mystifies me,
+
+Tell... me... why should it be?
+You have the power to hypnotize me...
+
+Let me live 'neath your spell,
+Do do that 'voodoo' that you do so well!
+For you do somethin' to me,
+That nobody else could do!
+
+That nobody else could do...
