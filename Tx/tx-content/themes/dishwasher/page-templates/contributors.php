1be6f5ef21456cdae21e58f858c89974274a14eb
Gupta Singh <bhaapsangya@yahoo.com>
2014-12-09 09:34:23 -0800

    20141209 - Updated test message.

--- Tx/tx-content/themes/dishwasher/page-templates/contributors.php
+++ Tx/tx-content/themes/dishwasher/page-templates/contributors.php
@@ -78,4 +78,54 @@ A literary celebrity by the age of 25, Goethe was ennobled by the Duke of Saxe-W
 
 After returning from a tour of Italy in 1788, his first major scientific work, the Metamorphosis of Plants, was published. In 1791 he was made managing director of the theatre at Weimar, and in 1794 he began a friendship with the dramatist, historian, and philosopher Friedrich Schiller, whose plays he premiered until Schiller's death in 1805. During this period Goethe published his second novel, Wilhelm Meister's Apprenticeship, the verse epic Hermann and Dorothea, and, in 1808, the first part of his most celebrated drama, Faust. His conversations and various common undertakings throughout the 1790s with Schiller, Johann Gottlieb Fichte, Johann Gottfried Herder, Alexander von Humboldt, Wilhelm von Humboldt, and August and Friedrich Schlegel have, in later years, been collectively termed Weimar Classicism.
 
-Arthur Schopenhauer cited Wilhelm Meister's Apprenticeship as one of the four greatest novels ever written[citation needed] and Ralph Waldo Emerson selected Goethe as one of six "representative men" in his work of the same name, along with Plato, Napoleon, and William Shakespeare. Goethe's comments and observations form the basis of several biographical works, most notably Johann Peter Eckermann's Conversations with Goethe. There are frequent references to Goethe's writings throughout the works of G. W. F. Hegel, Arthur Schopenhauer, Friedrich Nietzsche, Hermann Hesse, Thomas Mann, Sigmund Freud, and Carl Jung. Goethe's poems were set to music throughout the eighteenth and nineteenth centuries by a number of composers, including Wolfgang Amadeus Mozart, Ludwig van Beethoven, Franz Schubert, Robert Schumann, Johannes Brahms, Charles Gounod, Richard Wagner, Hugo Wolf, and Gustav Mahler.
+Arthur Schopenhauer cited Wilhelm Meister's Apprenticeship as one of the four greatest novels ever written[citation needed] and Ralph Waldo Emerson selected Goethe as one of six "representative men" in his work of the same name, along with Plato, Napoleon, and William Shakespeare. Goethe's comments and observations form the basis of several biographical works, most notably Johann Peter Eckermann's Conversations with Goethe. There are frequent references to Goethe's writings throughout the works of G. W. F. Hegel, Arthur Schopenhauer, Friedrich Nietzsche, Hermann Hesse, Thomas Mann, Sigmund Freud, and Carl Jung. Goethe's poems were set to music throughout the eighteenth and nineteenth centuries by a number of composers, including Wolfgang Amadeus Mozart, Ludwig van Beethoven, Franz Schubert, Robert Schumann, Johannes Brahms, Charles Gounod, Richard Wagner, Hugo Wolf, and Gustav Mahler.MASK BOY: Well, that’s a start.
+
+CHER: Well, what about your news?
+
+MASK BOY: Oh, it’s no big deal. I met a girl. We’re going out.
+
+CHER: Come on. Tell us.
+
+MASK BOY: Well, her name is Diana ….
+
+CHER: Yeah?
+
+MASK BOY: And she’s beautiful. She’s got long blond hair. (A friend whistles.) She rides horses and she’s beautiful and she’s smart and she loves me.
+
+CHER: What’s not to love, baby?
+
+SCENE 14 (OLD MAN enters his kitchen. Sitting at the table, he looks through a photo album filled with pictures of himself and his son, the Great Mutato as a child.. POLLIDORI enters, angry.)
+
+POLLIDORI: Tell me it isn’t true. You didn’t. You wouldn’t. Why?
+
+OLD MAN: Because I can.
+
+(POLLIDORI attacks OLD MAN)
+
+(Grunting as we see the shadows of the two men struggling, POLLIDORI strangling the OLD MAN.)
+
+(Commercial.)
+
+SCENE 15 (J.J.’S diner. MULDER enters. Everyone is hostile. MULDER defensive and over it. Exact opposite of earlier scene. LARGE MAN sticks his leg out as if to trip MULDER.)
+
+LARGE MAN: (sarcastically) Excuse me.
+
+MULDER: (also sarcastic) Not a problem.
+
+(Another hostile diner flips a spoonful of grits? at MULDER which lands on the back of his neck. MULDER reacts with disgust. He sits down and wipes the offending matter away. In the kitchen, J.J. spits loudly onto a plate of barely cooked eggs and hands it to the WAITRESS. The WAITRESS puts it down in front of MULDER.)
+
+MULDER: (looking at disgusting eggs) What’s this?
+
+WAITRESS: (saccharine) Compliments of J.J. Coffee?
+
+MULDER: (looking away) Sure.
+
+(WAITRESS spills hot coffee in MULDER’S lap. He jumps up, brushing off his crotch.)
+
+MULDER: Whoa! That’s not a place you want to burn a guy.
+
+(WAITRESS flounces away. MULDER looks as newspaper headline : "FBI Agents Say Monster A ‘Hoax’.")
+
+(Commotion outside. Lots of people running past the diner. MULDER follows them to where there is a large gathering outside the post office.)
+
+POSTAL MAN: You want to see your monster? 
